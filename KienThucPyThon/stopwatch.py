1baa9de3c337936901d7011966a946b8789ac6c7
onlycode-code <remchan725@gmail.com>
2026-09-06 22:57:29 +0700

    stopwatch

--- KienThucPyThon/stopwatch.py
+++ KienThucPyThon/stopwatch.py
@@ -14,6 +14,7 @@ class StopWatch(QWidget):
         self.initUI()
     def initUI(self):
         self.setWindowTitle("Stop Watch")
+        vbox = QVBoxLayout()
         vbox.addWidget(self.time_label)
         
         self.setLayout(vbox)
@@ -23,6 +24,8 @@ class StopWatch(QWidget):
         hbox.addWidget(self.start_button)
         hbox.addWidget(self.stop_button)
         hbox.addWidget(self.reset_button)
+
+        vbox.addLayout(hbox)
 
         self.setStyleSheet("""
             QPushButton,QLabel{
@@ -34,24 +37,36 @@ class StopWatch(QWidget):
                 font-size : 50px;
             }
             QLabel{
-                font-size : 50px;
+                font-size : 100px;
                 background-color : rgba(23, 135, 227,1);
                 border-radius : 20px;
             }
         
         """)
         self.start_button.clicked.connect(self.start)
-        self.start_button.clicked.connect(self.start)
-        self.start_button.clicked.connect(self.start)
+        self.stop_button.clicked.connect(self.stop)
+        self.reset_button.clicked.connect(self.reset)
         self.timer.timeout.connect(self.update_display)
 
     def start(self):
         self.timer.start(10)
     def stop(self):
-        self.timer.stop(10)
+        self.timer.stop()
     def reset(self):
-        pass
-if __name__ == "__main":
+        self.timer.stop()
+        self.time = QTime(0,0,0,0)
+        self.time_label.setText("00:00:00")
+    def format_time(self,time):
+        hours = time.hour()
+        minutes = time.minute()
+        seconds = time.second()
+        milliseconds = time.msec() // 10
+        return f"{hours:02}:{minutes:02}:{seconds:02}:{milliseconds:02}"
+    def update_display(self):
+        self.time = self.time.addMSecs(10)
+        self.time_label.setText(self.format_time(self.time))
+    
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     StopWatches = StopWatch()
     StopWatches.show()
